26379dcdd3d45098594cf391c40a4361ae9ed49c
Reymart A. Calicdan <reymart.calicdan@evsu.edu.ph>
2026-03-25 14:51:40 +0800

    fix errors

---
+++ src/Exceptions/RespawnRateLimitException.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Hibla\Parallel\Exceptions;
+
+/**
+ * Thrown when the pool's respawn rate limit is exceeded, indicating a crash loop.
+ *
+ * This exception causes a hard pool shutdown and is used to reject all pending
+ * and queued tasks when more workers restart within a one-second sliding window
+ * than the configured threshold allows.
+ *
+ * @see \Hibla\Parallel\Interfaces\ProcessPoolInterface::withMaxRestartPerSecond()
+ */
+final class RespawnRateLimitException extends \RuntimeException {}
